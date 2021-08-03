c3df5ceca6800bb48ba930936622f3ecd31652d5
Nick Grigoriadis <44061694+nikosgri@users.noreply.github.com>
2021-08-04 00:43:42 +0300

    term_options.h

--- toptions/src/toptions/term_options.h
+++ toptions/src/toptions/term_options.h
@@ -3,20 +3,20 @@
 
 
 
-/*
-*@name  set_block : Blocks reading operation
-*@param fd        : The accept file descriptor 
-*@param block     : Blocks reading or make read()  waits always for bytes 
-*@return		   : Nothing 
-*/
+/**
+ *@name  set_block : Blocks reading operation
+ *@param fd        : The accept file descriptor 
+ *@param block     : Blocks reading or make read()  waits always for bytes 
+ *@return		       : Nothing 
+ */
 void set_block(int fd,int block);
-/*
-*@name set_attributes : Setting Terminal attributes , baud rate speed and specifies if the parity bit is up or not.
-*@param  fd		   : The accept file descriptor
-*@param  speed        : in/out speed  baud rate
-*@param  parity       : Enable or disable parity bit
-*@return              : Nothing
-*/
+/**
+ *@name set_attributes : Setting Terminal attributes , baud rate speed and specifies if the parity bit is up or not.
+ *@param  fd		       : The accept file descriptor
+ *@param  speed        : in/out speed  baud rate
+ *@param  parity       : Enable or disable parity bit
+ *@return              : Nothing
+ */
 void set_attributes(int fd,int speed,int parity);
 
 
